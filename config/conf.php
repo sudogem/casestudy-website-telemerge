3b90bb5fd97f7010360e0cdde87d1f21c95a572c
Arman Ortega <ortegaaa@ph.ibm.com>
2017-06-20 00:18:01 +0800

    Update config/conf.php

--- config/conf.php
+++ config/conf.php
@@ -1,38 +1,23 @@
 <?php
 // database configuration
-// change the settings below that will match in your mysql database server 
+// change the settings below that will match in your mysql database server
 
-$conf['dbhost'] = 'localhost';
-$conf['dbusername'] = 'root';
-$conf['dbpassword'] = 'webdevel' ;
-$conf['dbdatabasename'] = 'teletrandb';
-
-
-
-
+// $conf['dbhost'] = 'localhost';
+// $conf['dbusername'] = 'root';
+// $conf['dbpassword'] = 'webdevel' ;
+// $conf['dbdatabasename'] = 'teletrandb';
 
 // for live site
-/*$conf['dbhost'] = 'localhost';
-$conf['dbusername'] = 'teletran_admin' ;
-$conf['dbpassword'] = 'admin147$' ;
-$conf['dbdatabasename'] = 'teletran_teledb'; */
-
-// max file size allocated 
+$conf['dbhost'] = 'us-cdbr-sl-dfw-01.cleardb.net';
+$conf['dbusername'] = 'b7616571a16ba9';
+$conf['dbpassword'] = 'b946dcbc';
+$conf['dbdatabasename'] = 'ibmx_98f1ba724131160';
+// max file size allocated
 $conf['max_voicefile_upload'] = 2000000 ;
-
-
 // mail
-//$conf['mail_from'] = 'brainwired@gmail.com';
-
 $conf['mail_from'] = 'support@telemerge.com';
-
-//$conf['mail_c_to'] = 'brainwired@gmail.com';
 $conf['mail_c_to'] = 'support@telemerge.com';
-
 $conf['mail_c_subject'] = 'Telemerge Contact Us Form';
-
-// $conf['mail_freetrial_to'] = 'brainwired@gmail.com';
 $conf['mail_freetrial_to'] = 'support@telemerge.com';
-
 $conf['mail_freetrial_subject'] = 'Telemerge Free Trial Form';
 ?>
