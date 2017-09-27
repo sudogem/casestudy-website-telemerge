4606c3ae062a52a214936e68199d32a9aeb66235
sudogem <sudogem@gmail.com>
2017-09-27 13:45:16 +0800

    Refactor the ff. pages: aboutus, contactus, hipaa, index

--- aboutus.php
+++ aboutus.php
@@ -8,309 +8,145 @@
 <meta name="description" content="Telemerge is a medical transcription company catering the increasing demand for accurate and effective data gathering and storage.The company is owned and operated by Telemerge Company Inc. A very young and promising corporation composed of tested and successful businessman in Cebu."  />
 <meta name="keywords" content="cebu medical transcription , medical transcription company , affordable medical transcription ,  philippine medical transcription , affordable medical transcription in cebu , transcriptionists , outsource medical transcription "  />
 <!-- InstanceBeginEditable name="doctitle" -->
-
 <title>About us - Telemerge Corporation</title>
-
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-
 <meta name="developer" content="Arman Ortega :: arman.ortega{at}yahoo{dot}com" />
-
 <meta name="robots" content="index , follow"  />
-
 <meta name="description" content="Telemerge is a medical transcription company catering the increasing demand for accurate and effective data gathering and storage.The company is owned and operated by Telemerge Company Inc. A very young and promising corporation composed of tested and successful businessman in Cebu."  />
-
 <meta name="keywords" content=" hipaa compliant website cebu , affordable medical transcription , philippine medical transcription , affordable medical transcription cebu , transcriptionists , cebu outsource medical transcription , philippine outsource medical transcription "  />
-
-
-
-
-
 <!-- InstanceEndEditable -->
 <link rel="stylesheet" href="css/style.css" media="screen" type="text/css"  />
 <script type="text/javascript" src="js/footer.js" language="javascript" ></script>
 <!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
 </head>
-
 <body>
 <div id= "container" >
   <div id="header"><span>Logo</span></div>
   <div id="slogan" >Telemerge Leading the World of Transcription...</div>
   <!-- InstanceBeginEditable name="topnav" -->
-
   <ul id="topnav">
-
     <li><a href="job.php">Job Opportunities</a> </li>
-
     <li><a href="clienttestimonial.php">Client Testimonials</a></li>
-
   </ul>
-
   <div class="clear"></div>
-
-
-
   <!-- InstanceEndEditable --><!-- InstanceBeginEditable name="nav" -->
-
   <ul id="nav" >
-
     <li><a href="index.php" id="cmdhome" >Home</a></li>
-
     <li><a href="aboutus.php" id="cmdabout" class="cmddefault">About Us</a></li>
-
     <li><a href="hipaa.php" id="cmdhipaa" >HIPAA</a></li>
-
     <li><a href="freetrial.php" id="cmdfreetrial" >Free Trial</a></li>
-
     <li><a href="contactus.php" id="cmdcontact"  >Contact Us</a></li>
-
     <li><a href="what_is_medical_transcription.php" id="cmdmt" >Medical Transcription</a></li>
-
     <li><a href="request_info.php" id="cmdrequest" >Request Info</a></li>
-
   </ul>
-
   <!-- InstanceEndEditable -->
   <div class="clear" ></div>
   <!-- InstanceBeginEditable name="front-content" -->
-
 <div id="column" >
-
 <h1 class="cap2 capline" >The Company</h1>
-
 <p class="text" >
-
 <img src="images/bldg-bottom_03.jpg" class="imageright"  />
-
-
-
 Telemerge is a medical transcription company catering the increasing demand for accurate and effective data gathering and storage.
-
 This is to answer the need of the medical practitioners whose time are so precious that making reports
-
 would be a bulk to them. Now, with the Telemerge Inc.  they can entrust there medical transcription
-
 work with no hassle. With the commitment of the company to the industry, we can assure that job will be delivered satisfactorily
-
 to the client.
-
 </p>
-
 <p class="text">
-
 The company is owned and operated by Telemerge Company Inc. A very young and promising corporation
-
 composed of tested and successful businessman in Cebu.
-
 </p>
-
 <p class="text">
-
 The business is located at the IT hub of Asia -- Cebu , in the beautiful island of Mactan, Lapu-lapu City.
-
 With 150 sq.m area condusive to office activity.
-
 </p>
-
 <br />
-
-
-
 <h1 class="cap2 capline" >Vision</h1>
-
 <p class="text">To Become The Global Leader In Transcription</p>
-
 <br />
-
-
-
 <h1 class="cap2 capline" >Mission</h1>
-
 <p class="text">
-
 Telemerge Is Aiming Global Recognition Through Its Quality, Personalized, Cost And
-
 Security Effective Medical Transcription Service.
-
 </p>
-
 <br />
-
-
-
 <h1 class="cap2 capline" >Commitments</h1>
-
 <p class="text">
-
 We are committed to deliver Quality of Service particularly in terms of:<br />
-
 </p>
-
 <div  class="staff-item" >
-
 <ul>
-
 <li>Spelling</li>
-
 <li>Format</li>
-
 <li>Grammar</li>
-
 <li>Punctuation</li>
-
 </ul>
-
 </div>
-
 <p class="text">
-
 We are committed to give Personalized Service to our clients by customizing format suitable for them.
-
 </p>
-
 <p class="text" >
-
 We are committed to maintain the status of being the one of the Cheapest Medical Transcriber.
-
 </p>
-
 <p class="text">
-
 We are committed to Continually Seek Human Resource Development Programs for Personal up-liftment of our employees.
-
 </p>
-
 <br />
-
 <h1 class="cap2 capline"  >The Team </h1>
-
 <div class="staff-item" >
-
   <h1>Jack Doe - <b>President</b></h1>
-
   <ul>
-
     <li>Banker - Far East Bank(1975-1979)</li>
-
     <li>Manager/Proprietor - Madera Auto Sales and Quick Auto Shop</li>
-
     <li>Bachelor of Science in Accountancy - University of San Jose Recoletos</li>
-
     <li>Residence: Cebu City Philippines</li>
-
     <li>Contact #: +1234567890</li>
-
   </ul>
-
 </div>
-
-
-
-
-
 <div class="staff-item" >
-
   <h1>Kim Bauer - <b>Vice-President</b></h1>
-
   <ul>
-
     <li>Marketing Manager - ALTEC Corporation</li>
-
     <li>General Manager - St. Francis Memorial Homes</li>
-
     <li>Residence: Dumlog, Talisay City</li>
-
     <li>Contact #: +1234567890</li>
-
   </ul>
-
 </div>
-
-
-
 <div class="staff-item" >
-
   <h1>Jack Bauer - <b>VP-Finance</b></h1>
-
   <ul>
-
     <li>Chief Financial Officer - St. Francis Memorial Homes</li>
-
     <li>Residence: N.Bacalso Ave., Cebu City</li>
-
     <li>Contact #: +1234567890</li>
-
   </ul>
-
 </div>
-
-
-
-
-
 <div class="staff-item" >
-
   <h1>John Doe - <b>CEO</b></h1>
-
   <ul>
-
     <li>Assistant Manager</li>
-
     <li>Bachelor of Science in Nursing - Colegio De San Lorenzo Ruiz De Manila</li>
-
     <li>Residence: Cebu City Philippines</li>
-
     <li>Contact #: +1234567890</li>
-
   </ul>
-
 </div>
-
-
-
 <div class="staff-item" >
-
   <h1>John Wick - <b>Chief Operation/Marketing Officer</b></h1>
-
   <ul>
-
     <li>Doctor of Naturopathic Medicine </li>
-
     <li>Residence: Willowdale , Ontario Canada </li>
-
     <li>Contact #: +1234567890</li>
-
   </ul>
-
 </div>
-
-
-
 <div class="staff-item" >
-
   <h1>Jane Doe - <b>Operation Manager </b></h1>
-
         <ul>
-
            <li>Doctor of Medicine - Matias H. Aznar Memorial College of Medicine</li>
-
     <li>Contact #: +1234567890</li>
-
-
-
         </ul>
-
 </div>
-
-
-
-
-
 </div>
-
   <!-- InstanceEndEditable -->
-
   <div class="clear" ></div>
-
 </div>
   <div id="footer" >
     <div id="wrapper" >
@@ -319,30 +155,21 @@ We are committed to Continually Seek Human Resource Development Programs for Per
         <h1>John Doe </h1>
         <h1>CEO, Telemerge Corporation </h1>
         <h1>Cebu  Philippines 6000</h1>
-        <h1>Email: johndoe@telemerge.com</h1>		
+        <h1>Email: johndoe@telemerge.com</h1>   
         <h1>Contact No: 1800-1234-5678</h1>
       </div>
-
         <!-- InstanceBeginEditable name="footnav" -->
-
         <ul id="footnav" >
-
                   <li><a href="terms.php">Terms of Use</a>&nbsp;|&nbsp;</li>
-
           <li><a href="privacy-policy.php">Privacy Policy</a>&nbsp;|&nbsp;</li>
-
           <li><a href="contactus.php">Contact Us</a>&nbsp;|&nbsp;</li>
-
           <li><a href="sitemap.php">Sitemap</a>&nbsp;|&nbsp;</li>
-
           </ul>
-
         <!-- InstanceEndEditable -->
         <div id="copyright" >
           <h1>Copyright &copy; 2006. Telemerge Corporation. All Rights Reserved.</h1>
         </div>
     </div>
   </div>
-
 </body>
 <!-- InstanceEnd --></html>
